d0f985ebc75cb7886e148d052955920d489cf5e0
tinysun21 <tinysun21@naver.com>
2018-03-03 23:01:54 +0900

    Apply ButterKnife (Modified)

--- app/src/main/java/com/tinysun/secretlottosix/FragmentGenerate.java
+++ app/src/main/java/com/tinysun/secretlottosix/FragmentGenerate.java
@@ -6,9 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 /**
@@ -41,6 +43,13 @@ public class FragmentGenerate extends Fragment {
         unbinder = ButterKnife.bind(this, view);
         return view;
     }
+
+    @OnClick(R.id.generate_btn)
+    public void onButtonClick(View view) {
+        Toast.makeText(getActivity(), "Generate button clicked!", Toast.LENGTH_SHORT).show();
+    }
+
+
 
     @Override public void onDestroyView() {
         super.onDestroyView();
--- app/src/main/java/com/tinysun/secretlottosix/FragmentPurchase.java
+++ app/src/main/java/com/tinysun/secretlottosix/FragmentPurchase.java
@@ -25,7 +25,7 @@ public class FragmentPurchase extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_history, container, false);
+        return inflater.inflate(R.layout.fragment_purchase, container, false);
     }
 
 }
